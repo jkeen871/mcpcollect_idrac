5dc6482c2c9977b757ee2f9b7372fcd586e48755
Jerry <jkeen871@gmail.com>
2019-05-14 10:37:11 -0500

    add output only option, streamline the script with an array loop

--- mcpcollect_idrac.sh
+++ mcpcollect_idrac.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+datestamp=`date '+%Y%m%d%H%M%S'`
+localbasedir="/tmp/mcpcollectIdrac-$USER/$datestamp"
+
 function usage {
         echo ""
 	echo "This tool is intended to be run from the CFG host in your MCP environment"
@@ -12,18 +16,25 @@ function usage {
 }
 
 
-while getopts "h:" arg; do
+declare -g IpmiCmds=( 	\
+			"racadm getsvctag" \
+			"racadm getsel" \
+			"racadm getraclog -c100" \
+			"racadm getsensorinfo" \
+			"racadm hwinventory" \
+		   )
+
+while getopts "h:o:" arg; do
         case $arg in
                 h) targetIdrac=("$OPTARG");;
+		o) outputFlag=true;outputfile="$localbasedir/$OPTARG";;
                 *) usage;;
                 \?) usage;;
         esac
 done
-
 if [ $OPTIND -eq 1 ]; then usage ; fi
 
-
-        sshOptions="-o StrictHostKeyChecking=no "
+        sshOptions="-o StrictHostKeyChecking=no"
         salttest=`sudo salt-call pillar.data maas:region:machines:$targetIdrac 2>/dev/null| tail -1`
         if [[ $salttest =~ .*$targetIdrac.* ]]; then
                 echo "Failed to get salt results for $targetIdrac"
@@ -35,15 +46,34 @@ if [ $OPTIND -eq 1 ]; then usage ; fi
                 echo "Verifying iDRAC version..."
                 dracver=$(sshpass -p $dracpw ssh $sshOptions $dracid@$dracip 'racadm getversion' | grep 'iDRAC Version'| awk '{print $1}')
                 if [[ "$dracver" == *"iDRAC"* ]]; then
-                        mkdir /tmp/mcpcollect_idrac 2> /dev/null
-                        sshpass -p $dracpw ssh $sshOptions $dracid@$dracip 'racadm getsvctag' > /tmp/mcpcollect_idrac/idrac_svctag_$targetIdrac.log
-                        sshpass -p $dracpw ssh $sshOptions $dracid@$dracip 'racadm getsel' > /tmp/mcpcollect_idrac/idrac_$targetIdrac.log
-                        sshpass -p $dracpw ssh $sshOptions $dracid@$dracip 'racadm getraclog -c100' > /tmp/mcpcollect_idrac/idrac_rac_$targetIdrac.log
-                        sshpass -p $dracpw ssh $sshOptions $dracid@$dracip 'racadm getsensorinfo' > /tmp/mcpcollect_idrac/idrac_sensor_$targetIdrac.log
-                        sshpass -p $dracpw ssh $sshOptions $dracid@$dracip 'racadm hwinventory' > /tmp/mcpcollect_idrac/idrac_hwinventory_$targetIdrac.log
-                else
-                        echo "Could not verify Dell iDrac"
-                fi
-        fi
-
+                        mkdir -p $localbasedir 2> /dev/null
+			if [ $outputFlag ]; then
+				echo "mkdir -p $localbasedir" >>$outputfile
+			fi
+			for ipmicmd in "${IpmiCmds[@]}"; 
+			do
+				getIpmi='echo -e "\n@@@=========$ipmicmd=====\n">> '$localbasedir'/idrac_'$targetIdrac'.log;sshpass -p '$dracpw' ssh '$sshOptions' '$dracid@$dracip' "'$ipmicmd'" >> '$localbasedir'/idrac_'$targetIdrac'.log'
+				if [ ! $outputFlag ]; then
+					echo "Collecting : $ipmicmd"
+					eval $getIpmi
+				else
+					echo 'echo "Collecting : '$ipmicmd'"' >> $outputfile
+					echo $getIpmi >> $outputfile
+				fi
+			done
+			if [ $outputFlag ]; then
+				chmod +x $outputfile
+				echo "Ouput script written to : "$outputfile
+				echo ""
+				echo "Copy this script to a node that has ssh open to the iDrac you want to collect from. Then execute it from there"
+				echo ""
+			else 
+				echo "Output written to : $localbasedir/idrac_$targetIdrac.log"
+			fi
+	
+                 else
+                         echo "Could not verify Dell iDrac"
+ 			exit
+                 fi
+	fi
 
