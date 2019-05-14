d196d083cb794f7fbf460051adf82efbd741f625
Jerry <jkeen871@gmail.com>
2019-05-14 12:10:01 -0500

    added GNU License

--- mcpcollect_idrac.sh
+++ mcpcollect_idrac.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 
+
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    <http://www.gnu.org/licenses/>.
+#
+
+
+
 datestamp=`date '+%Y%m%d%H%M%S'`
 localbasedir="/tmp/mcpcollectIdrac-$USER/$datestamp"
 
